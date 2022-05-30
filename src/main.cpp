eaeb513b373743ce4da44041f96e7917f3fc962f
chadd williams <will4614@pacificu.edu>
2022-05-29 22:38:56 -0700

    typo

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main()
     LabeledIntAnalyzer cLabledAnalyzer;
 
     cAnalyzer.findAverage("1,2,3,4", std::cout);
-    std::cout << endl;
+    std::cout << std::endl;
     cLabledAnalyzer.findAverage("age: 18, age: 19, age: 20", std::cout);
     return EXIT_SUCCESS;
 }
